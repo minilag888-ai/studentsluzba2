45d8b9de2765323094f4c1ab0804df70a1cf1600
minilag888-ai <minilag888@gmail.com>
2026-01-16 04:19:08 +0100

    ffdsfshhhfdf

---
+++ src/main/java/org/raflab/studsluzba/model/Nastavnik.java
@@ -0,0 +1,61 @@
+package org.raflab.studsluzba.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+import lombok.ToString;
+
+import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.Objects;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString(exclude = "zvanja")  // ← Isključi iz toString!
+public class Nastavnik {
+
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Long id;
+    private String ime;
+    private String prezime;
+    private String srednjeIme;
+    private String email;
+    private String brojTelefona;
+    private String adresa;
+
+    @OneToMany(mappedBy = "nastavnik", fetch = FetchType.LAZY)
+    @JsonIgnore
+    private Set<NastavnikZvanje> zvanja = new HashSet<>();
+
+    private LocalDate datumRodjenja;
+    private Character pol;
+    private String jmbg;
+
+    // Custom hashCode i equals BEZ "zvanja" polja!
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Nastavnik nastavnik = (Nastavnik) o;
+        return Objects.equals(id, nastavnik.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
---
+++ src/main/java/org/raflab/studsluzba/model/VrstaStudija.java
@@ -0,0 +1,53 @@
+package org.raflab.studsluzba.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "vrsta_studija")
+public class VrstaStudija {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "vrsta_studija_id")
+    private Long id;
+
+    @Column(name = "skraceni_naziv", nullable = false, unique = true, length = 10)
+    private String skraceniNaziv; // OAS, MAS, DAS, SSS...
+
+    @Column(name = "pun_naziv", nullable = false)
+    private String punNaziv; // Osnovne akademske studije, Master akademske studije...
+
+    // Constructors
+    public VrstaStudija() {
+    }
+
+    public VrstaStudija(String skraceniNaziv, String punNaziv) {
+        this.skraceniNaziv = skraceniNaziv;
+        this.punNaziv = punNaziv;
+    }
+
+    // Getters and Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getSkraceniNaziv() {
+        return skraceniNaziv;
+    }
+
+    public void setSkraceniNaziv(String skraceniNaziv) {
+        this.skraceniNaziv = skraceniNaziv;
+    }
+
+    public String getPunNaziv() {
+        return punNaziv;
+    }
+
+    public void setPunNaziv(String punNaziv) {
+        this.punNaziv = punNaziv;
+    }
+}
